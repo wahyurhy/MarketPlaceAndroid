79f45139eea49e15972bba7d799083600b2c320c
wahyurhy <wahyurahayu82@gmail.com>
2022-05-07 16:34:04 +0700

    add sharedPrefs

--- app/src/main/java/com/wahyurhy/marketplace/NavigationActivity.kt
+++ app/src/main/java/com/wahyurhy/marketplace/NavigationActivity.kt
@@ -1,6 +1,7 @@
 package com.wahyurhy.marketplace
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,6 +9,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.wahyurhy.marketplace.databinding.ActivityNavigationBinding
+import com.wahyurhy.marketplace.util.Prefs
 
 class NavigationActivity : AppCompatActivity() {
 
@@ -26,10 +28,28 @@ class NavigationActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications,
+                R.id.navigation_cart
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        navView.setOnItemSelectedListener {
+
+            if (it.itemId == R.id.navigation_notifications) {
+                val s = Prefs(this)
+                if (s.getIsLogin()) {
+                    Log.d("TAG", "Sudah Login")
+                } else {
+                    Log.d("TAG", "Belum Login, pindah ke menu Login")
+                }
+            } else {
+                Log.d("TAG", "onCreate: yg lain ${it.itemId}")
+            }
+
+            return@setOnItemSelectedListener true
+        }
     }
 }
